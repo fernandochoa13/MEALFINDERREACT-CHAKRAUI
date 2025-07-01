122c89b5aed343ed204f4b94c167747c95616ac6
fernandochoa13 <frosales1366@gmail.com>
2025-06-30 23:57:25 -0400

    Map ingredients

--- src/components/RecipeModalContent.tsx
+++ src/components/RecipeModalContent.tsx
@@ -1,11 +1,26 @@
-import { ModalBody, ModalCloseButton, ModalHeader, Image, Heading, Text } from "@chakra-ui/react"
+import { ModalBody, ModalCloseButton, ModalHeader, Image, Heading, Text, OrderedList, ListItem } from "@chakra-ui/react"
 import { MealDetails } from "../types"
 
 type Props = {
     data: MealDetails
 }
 
+const joinIngredientes = (data: MealDetails) => {
+    let ingredients = [];
+    for (let index = 1; index <= 20; index++){
+        const ingredient = data[`strIngredient${index}`];
+        const measure = data[`strMeasure${index}`];
+        if(ingredient !== "") {
+            ingredients.push(ingredient, measure)
+        }
+       
+    }
+    return ingredients;
+}
+
 function RecipeModalContent({ data }: Props) {
+   const ingredients = joinIngredientes(data);
+   console.log(ingredients);
   return (
     <>
     <ModalHeader>{data.strMeal}</ModalHeader>
@@ -13,6 +28,11 @@ function RecipeModalContent({ data }: Props) {
     <ModalBody>
         <Image alt={data.strMeal} width="100%" borderRadius="lg" src={data.strMealThumb}/>
         <Heading mt="4" mb="4" size="md">Ingredientes</Heading>
+        <OrderedList>
+            {ingredients.map((i) => (
+                <ListItem key={i}>{i}</ListItem>
+            ))}
+        </OrderedList>
         <Text whiteSpace="pre-line">{data.strInstructions}</Text>
     </ModalBody>
     
